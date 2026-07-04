321737e6409e531c55a9c3adb4ef41ea92ea97b8
TarannumMalik <maliktarannum883@gmail.com>
2026-07-04 17:38:29 +0530

    changed template id

--- js/form.js
+++ js/form.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 const EMAILJS_PUBLIC_KEY          = "dEu6NpYPedZYJO_Zs";
 const EMAILJS_SERVICE_ID          = "service_9k4ws9k";
 const EMAILJS_CONTACT_TEMPLATE_ID = "template_x1tndxd";
-const EMAILJS_NEWSLETTER_TEMPLATE_ID = "template_oobr9yp";
+const EMAILJS_NEWSLETTER_TEMPLATE_ID = "template_aot3cwt";
 
 emailjs.init({ publicKey: EMAILJS_PUBLIC_KEY });
 
